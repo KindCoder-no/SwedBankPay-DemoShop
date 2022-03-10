a4a322512222f5411eb76437e4c999f72171de83
Emre Sanden <emresanden@gmail.com>
2022-03-10 13:48:52 +0100

    Include Swedbank Pay Design guidelines

--- routes/cart.js
+++ routes/cart.js
@@ -71,6 +71,47 @@ router.get('/product/:id/add-cart', function (req, res) {
     }
 })
 
+router.get('/product/:id/add-cart/button', function (req, res) {
+    var product = ""
+    var session = req.session;
+
+    products.forEach(data => {
+        if(data.id === req.params.id){
+            product = data
+        }
+    })
+    if(product === ""){
+        // If product is not found, redirect to home page
+        res.send("No product")
+    }else{
+        if(session.cart){
+            var oldcart = session.cart
+
+            // Check if items is in cart
+            var oldcart_ids = []
+
+
+            oldcart.forEach(data => {
+                oldcart_ids.push(data.id)
+            })
+
+            if(oldcart_ids.includes(req.params.id)){
+                var oldcart_id = oldcart_ids.indexOf(req.params.id)
+                oldcart[oldcart_id].amount = Number(oldcart[oldcart_id].amount) + 1
+            }else{
+                var newcart = oldcart.push({id: req.params.id, amount: "1"})
+
+            }           
+            res.send("Success")
+        }else{
+            session.cart = [{id: req.params.id, amount: "1"}]
+
+            // Redirect to cart page
+            res.send("Success")
+        }
+    }
+})
+
 
 // Add to cart page
 router.get('/product/:id/remove-cart', function (req, res) {
